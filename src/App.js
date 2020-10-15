bbbfeeee115ebdc764f332668da92fab5043ec8a
Jonathan Won <jabinx92@gmail.com>
2020-10-14 22:05:37 -0700

    hhh

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Setup from './tutorial/2-useEffect/setup/3-useEffect-fetch-data';
+// import Final from './tutorial/1-useState/final/2-useState-basics';
+
 function App() {
   return (
     <div className='container'>
-      <h2>Advanced Tutorial</h2>
+      <Setup />
     </div>
   );
 }
