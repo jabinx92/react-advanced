f5f93b11e5bd6f5385cd649d8666d6ccea193b4b
Jonathan Won <jabinx92@gmail.com>
2020-10-15 20:29:16 -0700

    stepback

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Setup from './tutorial/3-conditional-rendering/setup/3-show-hide';
+import Setup from './tutorial/6-useReducer/setup'
 // import Final from './tutorial/1-useState/final/2-useState-basics';
 
 function App() {
